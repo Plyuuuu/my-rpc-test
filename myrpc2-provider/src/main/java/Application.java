eb50957f134ef5fff33eb69434496e9d556ba779
veikkoroc <veikkoroc@gmail.com>
2020-12-13 12:26:14 +0800

    first commit

---
+++ myrpc2-provider/src/main/java/Application.java
@@ -0,0 +1,7 @@
+/**
+ * @author Veikko Roc
+ * @version 1.0
+ * @date 2020/12/13 12:15
+ */
+public class Application {
+}
---
+++ myrpc2-provider/src/main/java/github/veikkoroc/impl/HelloWorldImpl.java
@@ -0,0 +1,17 @@
+package github.veikkoroc.impl;
+
+import github.veikkoroc.api.HelloWorld;
+
+/**
+ * 1.导入依赖后没有提示，会报错，要后续import
+ *
+ * @author Veikko Roc
+ * @version 1.0
+ * @date 2020/12/13 12:08
+ */
+public class HelloWorldImpl implements HelloWorld {
+    public String sayHello(String msg) {
+        System.out.println("github/veikkoroc/impl/HelloWorldImpl.java sayHello method excuse [msg:"+msg+"]");
+        return "Hello ~~~";
+    }
+}
